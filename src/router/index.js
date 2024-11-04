712b9be28c85dc4a8d4072af538f2592a40e9f43
Tran Duy <119783966+Tqduy2k3@users.noreply.github.com>
2024-11-04 23:55:10 +0700

    fix bug, create page error

--- src/router/index.js
+++ src/router/index.js
@@ -23,7 +23,11 @@ const routes = [
     component: () => import('@/views/ContactAdd.vue'),
     props: (route) => ({ contactId: route.params.id })
   },
-  
+  {
+    path: '/error',
+    name: 'ErrorPage',
+    component: () => import('@/views/ErrorPage.vue')
+  }
 ];
 
 const router = createRouter({
